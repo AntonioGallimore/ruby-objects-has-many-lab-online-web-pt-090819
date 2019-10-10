8d617a66ad5a08b4f89a18e89f1661077b923c3e
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-10 17:52:01 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,4 +7,21 @@ class Author
     @name = name
     @@all << self 
   end
-end 
+end def add_post(post)
+    song.artist = self 
+  end
+  
+  def add_post_by_name(name)
+    post = Post.new(name)
+    song.artist = self
+  end
+    
+    def post 
+    Post.all.select {|psot| post.author == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
