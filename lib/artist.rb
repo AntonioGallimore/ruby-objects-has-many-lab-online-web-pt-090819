53fc41e4c36ac1a5d914da9ca327a9ecc9c5dd2f
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-10 17:42:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,6 +21,12 @@ class Artist
     song.artist = self
   end
     
-  
+    def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
 
 end
