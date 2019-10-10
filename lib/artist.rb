45681dd176de155d1dd9c10e8bd396c63febce14
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-10 17:34:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,11 @@
 class Artist 
-  attr_accessor :name, :songs
+  attr_accessor :name
+  
+  @@all = []
   
   def initialize(name)
     @name = name
-    @songs = songs
-  end
-  
-  def songs(new)
-    @songs = songs
+    @@all << self 
   end
 
 end
